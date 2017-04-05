a08aa560ec04c14417591caa46f50eb5a411cec8
Diego Costa <diegobc@gmail.com>
2017-04-05 20:05:54 +0200

    removed imports

--- shapeshiftio/shapeshiftio.py
+++ shapeshiftio/shapeshiftio.py
@@ -1,9 +1,3 @@
-# try:
-#     # For Python 3.0 and later
-#     from urllib.request import urlopen
-# except ImportError:
-#     # Fall back to Python 2's urllib2
-#     from urllib2 import urlopen
 import requests
 import json
 
